0735c32869e0379c15b35b3352a9091ef94986fa
Brandon <bd1887@gmail.com>
2022-11-20 19:32:03 +0100

    Added SignalR Hub

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Server.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy => 
+        {
+            policy
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .AllowAnyHeader();
+        }
+    );
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" }
+    );
+});
 
 var app = builder.Build();
 
@@ -18,8 +40,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<GameHub>("/game");
 
 app.Run();
